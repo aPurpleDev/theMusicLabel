f42bcffdd2d782df3e6c4000cfd27ef5a9f7fe2b
M-Nasuk <m.kusanagi@gmx.fr>
2019-09-03 14:15:58 +0200

    fix name folders orders

--- skeleton/src/Controller/OrderLogController.php
+++ skeleton/src/Controller/OrderLogController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/order/log")
+ * @Route("/orders/log")
  */
 class OrderLogController extends AbstractController
 {
